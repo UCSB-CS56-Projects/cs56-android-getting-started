3a70a7e7e9f3712d825c29ed59842457e3130818
Pedro M. Sosa <pmsosa@umail.ucsb.edu>
2015-03-05 14:03:47 -0800

    fixed comments

--- docs/tutorial/3_Intro_to_Services/SimpleService/app/src/main/java/intro/android/cs56/ucsb/edu/simpleservice/MyService.java
+++ docs/tutorial/3_Intro_to_Services/SimpleService/app/src/main/java/intro/android/cs56/ucsb/edu/simpleservice/MyService.java
@@ -1,9 +1,5 @@
 package intro.android.cs56.ucsb.edu.simpleservice;
 
-/**
- * Created by TransAtlantic on 3/2/2015.
- */
-
 import android.app.Service;
 import android.util.Log;
 import android.widget.Toast;
